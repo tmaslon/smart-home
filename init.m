b20981bec1bb0fca3f2eeb4b0eff8d2c44417184
tmaslon <tmaso6@gmail.com>
2020-11-11 22:54:27 +0100

    Added intial Astronomical Clock implementation

--- init.m
+++ init.m
@@ -1,7 +1,9 @@
 timeStep = 5; % minutes
-days = 1;
+days = 2;
 
 stepsInDay = 24*60/timeStep;
 intervals = days*stepsInDay; % 288 5 minute intervals per 24hours
 
+sunrise = floor(0.25*stepsInDay);   
+sunset = floor(0.8*stepsInDay);
 %sim("test");
