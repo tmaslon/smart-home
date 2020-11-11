8fd0c609971b90740eb9b96b4ddad9561f67f9b7
tmaslon <tmaso6@gmail.com>
2020-11-11 17:13:56 +0100

    bugix

--- init.m
+++ init.m
@@ -2,6 +2,6 @@ timeStep = 5; % minutes
 days = 1;
 
 stepsInDay = 24*60/timeStep;
-intervals = days*288; % 288 5 minute intervals per 24hours
+intervals = days*stepsInDay; % 288 5 minute intervals per 24hours
 
 sim("test");
