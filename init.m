ef14f9bbc9d13164ddf4c778daf07b7a68a6398b
tmaslon <tmaso6@gmail.com>
2020-11-14 18:53:10 +0100

    Improved the Blinds Controller stateflow chart, joined rooms together to form a House subsystem, tweaks to the heating and blinds objects

--- init.m
+++ init.m
@@ -1,9 +1,13 @@
 timeStep = 5; % minutes
-days =1;
+days =2;
 
 stepsInDay = 24*60/timeStep;
 intervals = days*stepsInDay; % 288 5 minute intervals per 24hours
 
 sunrise = floor(0.25*stepsInDay);   
 sunset = floor(0.8*stepsInDay);
+
+%shutters thresholds
+T_low = -5; %open 
+T_high = 24; %close
 %sim("test");
